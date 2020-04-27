18d7da6165ae3cdc1c466dabb26893c9bc8ec7fa
Tylere Couture <tylerecouture@gmail.com>
2020-04-27 14:41:41 -0700

    Working buttons to open pdfs, email, and store page

--- panjwayi/scenes/title_screen/TitleScreen.gd
+++ panjwayi/scenes/title_screen/TitleScreen.gd
@@ -10,18 +10,21 @@ func _ready() -> void:
 
 func _on_Button_pressed(scene_to_load, uri_to_open):
 	print("BUTTON PRESSED!")	
-	if scene_path_to_load != null:
+	print("SCENE: ", scene_to_load)
+	print("URI: ", uri_to_open)
+	if scene_to_load != null:
 		scene_path_to_load = scene_to_load 
 		$FadeIn.show()
 		$FadeIn.fade_in()
-#	elif uri_to_open != null:
-##		var default_cursor = default_button.get_default_cursor_shape()
-##		default_button.set_default_cursor_shape(CURSOR_MOVE)
-#		print("OPENING URI: ", uri_to_open)
-#		OS.shell_open(uri_to_open)
-#		yield(get_tree().create_timer(0.5), "timeout")
-#		default_button.grab_focus()
-##		default_button.set_default_cursor_shape(default_cursor)
+	elif uri_to_open != null:
+#		var default_cursor = default_button.get_default_cursor_shape()
+#		default_button.set_default_cursor_shape(CURSOR_MOVE)
+		print("OPENING URI: ", uri_to_open)
+		var error = OS.shell_open(uri_to_open)
+		print(error)
+		yield(get_tree().create_timer(0.5), "timeout")
+		default_button.grab_focus()
+#		default_button.set_default_cursor_shape(default_cursor)
 		
 
 func _on_FadeIn_fade_finished() -> void:
