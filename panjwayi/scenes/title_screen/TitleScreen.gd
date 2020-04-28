7ae1928141113abeee31c523089af7f628e959ab
Tylere Couture <tylerecouture@gmail.com>
2020-04-27 20:28:53 -0700

    tweak logic in title screen script

--- panjwayi/scenes/title_screen/TitleScreen.gd
+++ panjwayi/scenes/title_screen/TitleScreen.gd
@@ -12,11 +12,11 @@ func _on_Button_pressed(scene_to_load, uri_to_open):
 	print("BUTTON PRESSED!")	
 	print("SCENE: ", scene_to_load)
 	print("URI: ", uri_to_open)
-	if scene_to_load != null:
+	if scene_to_load:
 		scene_path_to_load = scene_to_load 
 		$FadeIn.show()
 		$FadeIn.fade_in()
-	elif uri_to_open != null:
+	elif uri_to_open:
 #		var default_cursor = default_button.get_default_cursor_shape()
 #		default_button.set_default_cursor_shape(CURSOR_MOVE)
 		print("OPENING URI: ", uri_to_open)
